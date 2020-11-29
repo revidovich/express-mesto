9a6e5681cc99050812e40538e89506834a54a4dc
Anna <nutkatuz@yandex.ru>
2020-11-29 13:37:20 +0500

    bugfix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,15 @@
-// авторизационный мидлвэр для проверки JWT. для авторизации/ должен верифицировать токен из заголовков.
-// OK- мидлвэр должен добавлять пейлоуд токена в объект запроса
-
 const jwt = require('jsonwebtoken');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
-const jwtSign = require('../helpers/jwt-sign')
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log('------------------' + authorization);
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
-// в ост случаях:
   const token = authorization.replace('Bearer ', '');
-  console.log('MYTOKENIS' + token);
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'admin-secret');
@@ -25,6 +19,5 @@ module.exports = (req, res, next) => {
       .send({ message: 'Нет прав доступа' });
   }
   req.user = payload; // записываем пейлоуд в объект запроса
-  console.log('MYPAYLOADIS' + payload);
-  next(); // пропускаем запрос дальше
+  return next(); // пропускаем запрос дальше
 };
