fa2e820fb261fba96e307be8ca6f6bfa7c8dca50
Anna <nutkatuz@yandex.ru>
2020-12-10 15:19:10 +0500

    feature: like dislake card

--- controllers/cards.js
+++ controllers/cards.js
@@ -55,9 +55,72 @@ const getCardById = (req, res, next) => {
     .catch(next);
 };
 
+// const likeCard = (req, res, next) => { // /cards/likes/:_id
+//   Card.findByIdAndUpdate(req.params._id, { likes: req.user._id }, { new: true })
+//     .then((card) => {
+//       // console.log(req)
+//       if (!card) {
+//         throw new NotFoundError('Нет карточки с таким id');
+//       }
+//       return res.send(card);
+//     })
+//     .catch(next);
+// }; мой айдишник user 5fbe60db589d5f1f4cbc748a
+
+// const likeCard = (req, res, next) => {
+//   console.log(req.user);
+//   Card.findByIdAndUpdate(req.params._id, { $addToSet: { likes: req.user._id } }, { new: true })
+//     .orFail(new NotFoundError('Нет карточки с таким id'))
+//     .then((card) => {
+//       res.status(200).send(card);
+//     })
+//     .catch(next);
+// };
+
+const likeCard = async (req, res, next) => {
+  try {
+    const likedCard = await Card.findByIdAndUpdate(
+      req.params._id,
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    );
+    if (!likedCard) throw new NotFoundError('Карточка не найдена');
+    res.status(200).send(likedCard);
+  } catch (error) {
+    next(error);
+  }
+};
+const dislikeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(
+    req.params._id,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(new NotFoundError('Нет карточки с таким id'))
+    .then((cards) => {
+      res.send(cards);
+    })
+    .catch(next);
+};
+
+// // Card это объект {"likes":[],"createdAt":"2020-12-02T15:57:41.604Z","_id":"5fc7e7c1949d1604686b7b18","name":"pictures","link":"https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg","owner":"5fbe60db589d5f1f4cbc748a","__v":0}
+
+// const dislikeCard = (req, res, next) => {
+//   Card.update(req.params._id, { likes: 'req.user._id' }, { likes: '' })
+//     .then((card) => {
+//       if (!card) {
+//         throw new NotFoundError('Нет карточки с таким id');
+//       }
+//       return res.send(card);
+//     })
+//     .catch(next);
+// };
+
 module.exports = {
   getCards,
   postCard,
   deleteCard,
   getCardById,
+  likeCard,
+  dislikeCard,
 };
